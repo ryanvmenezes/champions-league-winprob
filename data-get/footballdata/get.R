ae864b124e6e69178796134612412e436e75b345
ryanvmenezes <ryanvmenezes@gmail.com>
2019-09-25 12:50:40 -0700

    leg info script

--- data-get/footballdata/get.R
+++ data-get/footballdata/get.R
@@ -1,14 +1,15 @@
 library(httr)
 library(tidyverse)
 
-seasons = 2016:2001
+seasons = 2020:2001
 
 for (s in seasons) {
+  comp = 'PL'
   req = GET(
-    str_c('https://api.football-data.org/v2/competitions/CL/matches?season=', s),
+    str_c('https://api.football-data.org/v2/competitions/', comp, '/matches?season=', s),
     add_headers('X-Auth-Token' = '87aa684e84a947daac79132d8180e2c0')
     )
-  writeBin(content(req, 'raw'), str_c('data/footballdata/raw/', s, '.json'))
+  writeBin(content(req, 'raw'), str_c('data/footballdata/raw/', comp, '-', s, '.json'))
   print(str_c('done with ', s))
   Sys.sleep(10)
 }
--- data-get/footballdata/matches.R
+++ data-get/footballdata/matches.R
@@ -2,15 +2,19 @@ library(purrr)
 library(jsonlite)
 library(tidyverse)
 
-json = fromJSON('data/footballdata/raw/2017.json')
+json = fromJSON('data/footballdata/raw/CL-2019.json')
 
 names(json)
 
 str(json, max.level = 1)
 str(json$matches, max.level = 1)
 
-matches = json$matches %>% flatten()
-str(matches, max.level = 1)
+matches = json$matches %>% 
+  flatten() %>% 
+  as_tibble()
+matches %>% head()
+
+
 
 matchinfo = matches %>%
   select(id, date = utcDate, stage,
@@ -25,9 +29,68 @@ matchinfo = matches %>%
          scoreHpens = replace_na(scoreHpens, 0),
          scoreApens = replace_na(scoreApens, 0),
          scoreH90 = (scoreH - scoreHet),
-         scoreA90 = (scoreA - scoreAet)) %>% 
-  as_tibble() %>% 
+         scoreA90 = (scoreA - scoreAet)) %>%
   select(id:scoreA, scoreH90, scoreA90, everything()) %>% 
-  mutate(tieid = map2_chr(idH, idA, ~str_c(sort(c(.x, .y)), collapse = '|')))
+  mutate(tieid = map2_chr(idH, idA, ~str_c(sort(c(.x, .y)), collapse = '|'))) %>% 
+  arrange(date) %>% 
+  group_by(stage, tieid) %>% 
+  mutate(tieno = rank(date))
 
-matchinfo
+View(matchinfo)
+
+# lfc games
+matchinfo %>% filter(str_detect(tieid, '^64\\||\\|64$')) %>% View()
+
+matches %>% count(stage)
+
+twolegmatches = matchinfo %>% 
+  filter(stage %in% c("1ST_QUALIFYING_ROUND","2ND_QUALIFYING_ROUND","3RD_QUALIFYING_ROUND",
+                      "PLAY_OFF_ROUND","ROUND_OF_16","QUARTER_FINALS","SEMI_FINALS"))
+
+twolegs = full_join(twolegmatches %>% filter(tieno == 1) %>% select(-tieno),
+                    twolegmatches %>% filter(tieno == 2) %>% select(-tieno),
+                    by = c('stage','tieid'),
+                    suffix = c('.g1','.g2')) %>%
+  mutate(goals1 = scoreH.g1 + scoreA.g2,
+         goals2 = scoreA.g1 + scoreH.g2,
+         awaygoals1 = scoreA.g2,
+         awaygoals2 = scoreA.g1,
+         goals1ft = scoreH.g1 + scoreA90.g2,
+         goals2ft = scoreA.g1 + scoreH90.g2,
+         awaygoals1ft = scoreA90.g2,
+         awaygoals2ft = scoreA.g1,
+         goals1et = scoreAet.g2,
+         goals2et = scoreHet.g2,
+         pens1 = scoreApens.g2,
+         pens2 = scoreHpens.g2) %>% 
+  select(stage, tieid,
+         team1id = idH.g1, team2id = idA.g1,
+         team1 = teamH.g1, team2 = teamA.g1, 
+         goals1:pens2) %>% 
+  mutate(
+    winner = case_when(
+      goals1 > goals2 ~ team1,
+      goals2 > goals1 ~ team2,
+      goals1 == goals2 ~ case_when(
+        awaygoals1 > awaygoals2 ~ team1,
+        awaygoals2 > awaygoals1 ~ team2,
+        awaygoals1 == awaygoals2 ~ case_when(
+          pens1 > pens2 ~ team1,
+          pens2 > pens1 ~ team2,
+        )
+      )
+    ),
+    aet = (goals1ft == goals2ft) & (awaygoals1ft == awaygoals2ft),
+    agr = (goals1 == goals2) & (awaygoals1 != awaygoals2),
+    pk = (goals1 == goals2) & (awaygoals1 == awaygoals2),
+    result = str_c(
+      team1,' (', goals1, '-', goals2,
+      case_when(aet ~ ' aet) ', TRUE ~ ') '),
+      team2,
+      case_when(
+        pk ~ str_c(', ', winner, ' won on penalties'),
+        agr ~ str_c(', ', winner, ' won on away goals'),
+        TRUE ~ ''
+      )
+    )
+  )
