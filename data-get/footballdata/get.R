7894411ec48884bfa32e7d82483d9d3ea1017a61
ryanvmenezes <ryanvmenezes@gmail.com>
2019-09-27 20:02:23 -0700

    latest

--- data-get/footballdata/get.R
+++ data-get/footballdata/get.R
@@ -3,13 +3,18 @@ library(tidyverse)
 
 seasons = 2020:2001
 
-for (s in seasons) {
-  comp = 'PL'
-  req = GET(
-    str_c('https://api.football-data.org/v2/competitions/', comp, '/matches?season=', s),
-    add_headers('X-Auth-Token' = '87aa684e84a947daac79132d8180e2c0')
-    )
-  writeBin(content(req, 'raw'), str_c('data/footballdata/raw/', comp, '-', s, '.json'))
-  print(str_c('done with ', s))
-  Sys.sleep(10)
+getandsave <- function(comp, resource, szn) {
+  destfile = str_c('data-get/footballdata/raw/', comp, '-', resource, '-', szn, '.json')
+  if (!file.exists(destfile)) {
+    req = GET(
+      str_c('https://api.football-data.org/v2/competitions/', comp, '/', resource, '?season=', szn),
+      add_headers('X-Auth-Token' = '87aa684e84a947daac79132d8180e2c0'))
+    writeBin(content(req, 'raw'), destfile)
+    print(str_c(destfile, ' downloaded'))
+  } else {
+    print(str_c(destfile, ' already exists'))
+  }
 }
+
+getandsave('CL', 'matches', 2019)
+getandsave('CL', 'teams', 2019)
