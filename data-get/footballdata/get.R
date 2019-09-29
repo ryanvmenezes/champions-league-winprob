c62667f5927a0f094cb94506b3e6c165257a31a7
Ryan Menezes <ryanvmenezes@gmail.com>
2019-09-28 19:25:48 -0700

    saving

--- data-get/footballdata/get.R
+++ data-get/footballdata/get.R
@@ -1,20 +1,28 @@
+library(here)
 library(httr)
 library(tidyverse)
 
-seasons = 2020:2001
-
 getandsave <- function(comp, resource, szn) {
-  destfile = str_c('data-get/footballdata/raw/', comp, '-', resource, '-', szn, '.json')
+  fname = str_c(comp, '-', resource, '-', szn, '.json')
+  destfile = here('data-get', 'footballdata', 'raw', fname)
   if (!file.exists(destfile)) {
     req = GET(
       str_c('https://api.football-data.org/v2/competitions/', comp, '/', resource, '?season=', szn),
       add_headers('X-Auth-Token' = '87aa684e84a947daac79132d8180e2c0'))
     writeBin(content(req, 'raw'), destfile)
-    print(str_c(destfile, ' downloaded'))
+    print(str_c(fname, ' downloaded'))
   } else {
-    print(str_c(destfile, ' already exists'))
+    print(str_c(fname, ' already exists'))
   }
+  # Sys.sleep()
 }
 
-getandsave('CL', 'matches', 2019)
-getandsave('CL', 'teams', 2019)
+leagues = c('CL') # 'EL'
+resources = c('matches','teams','standings')
+seasons = 2019:2017
+
+allrequests = expand.grid(comp = leagues,
+                          resource = resources,
+                          szn = seasons,
+                          stringsAsFactors = FALSE)
+pwalk(allrequests, getandsave)
