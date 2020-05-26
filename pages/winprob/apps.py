693409be61605572383727e7f5a6ca6f9ac04127
Ryan Menezes <ryanvmenezes@gmail.com>
2020-05-26 01:20:16 -0700

    getting started on site

---
+++ pages/winprob/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WinprobConfig(AppConfig):
+    name = 'winprob'
---
+++ pages/winprob/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Competition(models.Model):
+    COMPETITION_CHOICES = [
+        ('cl', 'UEFA Champions League'),
+        ('el', 'UEFA Europa League'),
+    ]
+    competition_name = models.CharField(
+        max_length=5,
+        choices=COMPETITION_CHOICES
+    )
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    fbrefid = models.CharField(max_length=10)
+    # other_names = models.CharField(max_length=100)
+
