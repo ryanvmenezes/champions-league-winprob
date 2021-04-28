fa6a8b3688334d751a58222f72870dacb5640a4f
ryanvmenezes <ryanvmenezes@gmail.com>
2021-04-28 02:02:04 -0700

    top live bar

--- pages/winprob/context_processors.py
+++ pages/winprob/context_processors.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from winprob.models import Tie
 
 def live_ties(request):
+    live_ties = Tie.objects.filter(in_progress=True)
+    if len(live_ties) > 4:
+        live_ties = live_ties[:4]
     return {
-        "live_ties": Tie.objects.filter(in_progress=True)[:4],
+        "live_ties": live_ties,
     }
